b54f04a033f730ee2c64beee05cb0c6738a54c69
delm83 <delm83@googlemail.com>
2025-05-03 13:24:10 +0100

    added strategy for quincy bot

--- RPS.py
+++ RPS.py
@@ -26,7 +26,18 @@ def player(prev_play, opponent_history=[], guess = ''):
               'P': 'R',
               'S': 'P',
           }):
+            
             return 'R' if len(opponent_history) == 3 else next_in_sequence[prev_play]
+    
+    def quincy_strategy(next_in_sequence={
+              'RR': 'P',
+              'RP': 'P',
+              'PP': 'S',
+              'PS': 'R',
+              'SR': 'R',
+          }):
+
+            return next_in_sequence[last_two]
     
     best_response = {'P': 'S', 'R': 'P', 'S': 'R'}
     
@@ -34,6 +45,8 @@ def player(prev_play, opponent_history=[], guess = ''):
         guess = abbey_strategy()
     elif first_two == 'PR':
         guess = kris_strategy()
+    elif first_three == 'RPP':
+        guess = quincy_strategy()
     else:
         guess = 'P'
         
