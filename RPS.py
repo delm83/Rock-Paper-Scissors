c948b4d442cd4d851fd2c12e375d9b6c79840ed3
delm83 <delm83@googlemail.com>
2025-06-19 19:50:52 +0100

    Added Markov chain function to allow me to beat abbey and mrugesh more than once

--- RPS.py
+++ RPS.py
@@ -1,11 +1,13 @@
-def player(prev_play, opponent_history=[], best_response = {'P': 'S', 'R': 'P', 'S': 'R'}):
+def player(prev_play, opponent_history=[], best_response = {'P': 'S', 'R': 'P', 'S': 'R'}, abbey_counter=[0]):
 
+    guess = 'P'
     opponent_history.append(prev_play)
     first_two = "".join(opponent_history[1:3])
     last_two = "".join(opponent_history[-2:])
+    last_three = "".join(opponent_history[-3:])
 
     def abbey_strategy(next_in_sequence={
-          # Abbey follows a predictable pattern if I do too
+          # First time I play Abbey she follows a predictable pattern if I do too
               'RR': 'S',
               'RP': 'S',
               'RS': 'S',
@@ -16,7 +18,8 @@ def player(prev_play, opponent_history=[], best_response = {'P': 'S', 'R': 'P', 
               'SP': 'P',
               'SS': 'P',
           }):
-
+            if len(opponent_history) == 1000:
+                abbey_counter[0] += 1
             return next_in_sequence[last_two]
     
     def kris_strategy(next_in_sequence={
@@ -25,7 +28,6 @@ def player(prev_play, opponent_history=[], best_response = {'P': 'S', 'R': 'P', 
               'P': 'R',
               'S': 'P',
           }):
-            
             return 'R' if len(opponent_history) == 3 else next_in_sequence[prev_play]
     
     def quincy_strategy(next_in_sequence={
@@ -36,31 +38,100 @@ def player(prev_play, opponent_history=[], best_response = {'P': 'S', 'R': 'P', 
               'PS': 'R',
               'SR': 'R',
           }):
-
             return next_in_sequence[last_two]
     
     def mrugesh_strategy():
-               # continually loop through all 3 options to ensure last 10 of my guesses will have 4 of one option and 3 of the other 2 options each round
-               # for example [S,R,P,S,R,P,S,R,P,S] (4XS) will be followed by [R,P,S,R,P,S,R,P,S,R] (4XR)
-               # this ensures that there is only one option with max frequency each round which will change each round so mrugesh will also change option each round
+        # This method works the first time I play mrugesh
+        # continually loop through all 3 options to ensure last 10 of my guesses will have 4 of one option and 3 of the other 2 options each round
+        # for example [S,R,P,S,R,P,S,R,P,S] (4XS) will be followed by [R,P,S,R,P,S,R,P,S,R] (4XR)
+        # this ensures that there is only one option with max frequency each round which will change each round so mrugesh will also change option each round
                if len(opponent_history)%3 == 2:
                     return 'R'
                elif len(opponent_history)%3 == 1:
                     return 'P'
                else:
                     return 'S'
+
+    # markov chain which allows me to beat Abbey and Mrugesh twice in a row           
+    def markov_strategy(play_order=[{
+              "RRR": 0,
+              "RRP": 0,
+              "RRS": 0,
+              "RPR": 0,
+              "RPP": 0,
+              "RPS": 0,
+              "RSR": 0,
+              "RSP": 0,
+              "RSS": 0,
+              "PRR": 0,
+              "PRP": 0,
+              "PRS": 0,
+              "PPR": 0,
+              "PPP": 0,
+              "PPS": 0,
+              "PSR": 0,
+              "PSP": 0,
+              "PSS": 0,
+              "SRR": 0,
+              "SRP": 0,
+              "SRS": 0,
+              "SPR": 0,
+              "SPP": 0,
+              "SPS": 0,
+              "SSR": 0,
+              "SSP": 0,
+              "SSS": 0,
+              "SRR": 0,
+              "SRP": 0,
+              "SRS": 0,
+              "SPR": 0,
+              "SPP": 0,
+              "SPS": 0,
+              "SSR": 0,
+              "SSP": 0,
+              "SSS": 0,
+          }]):
+        
+        if len(last_three) == 3:
+            play_order[0][last_three] += 1
+
+        potential_plays = [
+            "RR" + prev_play,
+            "RP" + prev_play,
+            "RS" + prev_play,
+            "PR" + prev_play,
+            "PP" + prev_play,
+            "PS" + prev_play,
+            "SR" + prev_play,
+            "SP" + prev_play,
+            "SS" + prev_play,
+        ]
+
+        sub_order = {
+            k: play_order[0][k]
+            for k in potential_plays if k in play_order[0]
+            }
+
+        prediction = max(sub_order, key=sub_order.get)[-1:]
+        # break up my pattern of play occassionally to outmaneuver my opponent's pattern finding
+        return 'S' if len(opponent_history) % 5 == 0 else prediction
     
     # Deduce opponent based on opponent's first moves
-    if len(opponent_history)<3:
-         guess = 'P'
-    elif first_two == 'PP':
-        guess = abbey_strategy()
-    elif first_two == 'PR':
-        guess = kris_strategy()
-    elif first_two == 'RR':
-        guess = mrugesh_strategy()
-    else:
-        guess = quincy_strategy()
+    if len(opponent_history)>2:
+        if first_two == 'RP':
+            guess = quincy_strategy()
+        elif first_two == 'PR':
+            guess = kris_strategy()
+        # first time I play mrugesh he opens RR and I can beat him with my mrugesh strategy
+        # second time I play mrugesh he opens SS and I need markov strategy to guarantee victory
+        elif first_two == 'RR':
+            guess = mrugesh_strategy()
+        elif first_two == 'SS':
+            guess = markov_strategy()
+        # first time I play abbey I can beat her with my abbey strategy 
+        # second time I play abbey I need markov strategy to guarantee victory
+        else:
+            guess = abbey_strategy() if abbey_counter[0]==0 else markov_strategy()
         
     # clear opponent history between each player
     if len(opponent_history) == 1000:
